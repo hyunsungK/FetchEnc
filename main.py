b005ed8b5fa1ea9fc99915e77c15586e521c0c92
yunseoChang <yunseo.chang@avikus.ai>
2021-07-15 17:04:30 +0900

    fix: Zone No Calculation

--- main.py
+++ main.py
@@ -9,23 +9,21 @@ import threading
 
 
 def get_temp_Enc_Data(cursor):
-    cursor.execute(F'Select * from "ENCData" limit 10')
+    cursor.execute(
+        F'Select * from "ENCData" where "Location Lat" is not null limit 10')
     next_rows = cursor.fetchall()
     return next_rows
 
 
-dlatitude = [0, -1, 0, 1, 1, 1, 0, -1, -1]
-dlongitude = [0, 1, 1, 1, 0, -1, -1, -1, 0]
+dlatitude = [0, 1, 1, 1, 0, -1, -1, -1, 0]
+dlongitude = [0, -1, 0, 1, 1, 1, 0, -1, -1]
 
 
-def calulate_Zone_No(latitude, longitude, factor: 5):
+def calculate_Zone_No(latitude, longitude):
     latitude = latitude + 90
     longitude = longitude + 180
 
-    column = math.trunc(latitude)
-    row = math.trunc(longitude)
-
-    return column + row * 360
+    return math.trunc(longitude) + math.trunc(latitude) * 360
 
 
 def get_Zone_Enc_Data(cursor, zone_no):
@@ -36,36 +34,37 @@ def get_Zone_Enc_Data(cursor, zone_no):
 
 def main():
     try:
+        start_time = time.time()
         connect = psycopg2.connect(
-            host='localhost', dbname='enc', user='postgres')
+            host='localhost', dbname='new_enc', user='postgres')
         cursor = connect.cursor()
+        print(F'taken time:{time.time()-start_time}s')
 
         # 임시 구현.
         temp_next_enc_data = get_temp_Enc_Data(cursor)
-        index = 0
         ##
 
         cur_Latitude = 32
         cur_Longitude = 127
         cur_Zone_No = 150
 
-        for temp_next_data in temp_next_enc_data:  # while문으로 대체 예정
-            next_latitude = temp_next_data["Location_Lat"]
-            next_longitude = temp_next_data["Location_Lon"]
-            print(next_latitude, next_longitude)
-
+        for temp in temp_next_enc_data:  # while문으로 대체 예정
+            next_latitude = temp[6]
+            next_longitude = temp[7]
             next_Zone_No = calulate_Zone_No(next_latitude, next_longitude)
-            print(next_latitude, next_longitude, next_Zone_No)
 
             if cur_Zone_No == next_Zone_No:
                 next_Zone_No = cur_Zone_No
                 next_latitude = cur_Latitude
                 next_longitude = cur_Longitude
-
             # TODO: 9 hot zone, 7 broad zone. Total: 16 zone to redis
-            # else:
-            #     for i in range(0, 9):
-            #         hot_zone_no = (math.floor(next_latitude) + dlatitude.index(i)) * ((math.floor(next_latitude) + dlongitude[i])
+            else:
+                temp_l = []
+                for i in range(0, 9):
+                    hot_zone_no = next_Zone_No + 360 * \
+                        dlatitude[i] + dlongitude[i]
+                    temp_l.append(hot_zone_no)
+                print(temp_l)
 
     except Exception as e:
         print("Error", e)
@@ -73,8 +72,8 @@ def main():
         # print(len(Hot_Zone_Enc_Data))
         pass
     finally:
-        if conn:
-            conn.close()
+        if connect:
+            connect.close()
 
 
 if __name__ == '__main__':
